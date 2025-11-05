4b3edbe5c0c64f03e4d56ab8197cfd68b05f94e7
LucasFriske <lucasfriske@hotmail.com>
2025-11-05 09:34:59 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>  
+    <head>  
+         <title>Gerador de Currículo</title>
+        <meta charset="UTF-8">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  
+        <script src="_JavaScript/script.js"></script>
+
+        <link rel="stylesheet" href="style.css">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        
+    </body>
+</html>
